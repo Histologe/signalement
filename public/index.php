77c50dbc4462b10740b5dce530a8e6a23bf3b8b3
Sam BZEZ <sam@bzez.dev>
2022-03-09 15:17:19 +0100

    Init...

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     /*dump($_SERVER['REMOTE_ADDR']);*/
-    if($_SERVER['REMOTE_ADDR'] === '176.188.79.165')
+    if($_SERVER['REMOTE_ADDR'] === '37.166.79.65')
     {
         $context['APP_ENV'] = 'dev';
         $context['APP_DEBUG'] = true;
