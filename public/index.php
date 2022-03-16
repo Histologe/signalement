948fe8666ed19f85bd1991d2c8e9e0157b7d349d
Sam BZEZ <sam@bzez.dev>
2022-03-17 00:39:22 +0100

    Init...

--- public/index.php
+++ public/index.php
@@ -6,10 +6,10 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     /*dump($_SERVER['REMOTE_ADDR']);*/
-    if($_SERVER['REMOTE_ADDR'] === '37.165.161.217')
+/*    if($_SERVER['REMOTE_ADDR'] === '37.165.161.217')
     {
         $context['APP_ENV'] = 'dev';
         $context['APP_DEBUG'] = true;
-    }
+    }*/
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
