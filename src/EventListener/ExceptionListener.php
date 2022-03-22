e807a0d8dcbe30f3aca4e2400e882fda01f9fce3
Sam BZEZ <sam@bzez.dev>
2022-03-22 22:44:05 +0100

    Init...

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,9 +3,12 @@
 namespace App\EventListener;
 
 use App\Service\NotificationService;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+use function PHPUnit\Framework\isEmpty;
 
 class ExceptionListener
 {
@@ -19,11 +22,20 @@ class ExceptionListener
     public function onKernelException(ExceptionEvent $event)
     {
         if ($event->getRequest()->get('signalement') !== null) {
+            $attachment =['documents'=>0,'photos'=>0];
+            if($files = $event->getRequest()->files->get('signalement')) {
+                foreach ($files as $k=> $file) {
+                    foreach ($file as $file_) {
+                        $attachment[$k]++;
+                    }
+                }
+            }
             $this->notificationService->send(NotificationService::TYPE_ERREUR_SIGNALEMENT, 'sam@bzez.dev', [
                 'url' => $_SERVER['SERVER_NAME'],
                 'code' => $event->getThrowable()->getCode(),
                 'error' => $event->getThrowable()->getMessage(),
-                'signalement' => $event->getRequest()->get('signalement')
+                'signalement' => $event->getRequest()->get('signalement'),
+                'attachment' => $attachment
             ]);
         }
     }
