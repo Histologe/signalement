8af3228069adf440d001baec97a8321b9144154c
Sam BZEZ <sam@bzez.dev>
2022-03-22 15:56:09 +0100

    Revert "Init..."
    This reverts commit e1469ff8ee1e5ebbdf8642c3aff245a9fe92be94.

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -11,10 +11,6 @@ class ExceptionListener
 {
     private NotificationService $notificationService;
 
-    const ERROR_500 = ['title'=>'Une erreur est survenue...','icon'=>'icon-bug-lg','message'=>"Une erreur inattendue s'est produite ! Notre équipe fait tout pour la corriger au plus vite.<br>Veuillez réessayer plus tard ou cliquer sur le bouton ci-dessous pour retourner à l'accueil."];
-    const ERROR_403 = ['title'=>'Accès refusé','icon'=>'icon-stop-lg','message'=>"Vous n'avez pas les droits nécessaires pour accéder à cette page.<br>Cliquez sur le bouton ci-dessous pour retourner à l'accueil."];
-    const ERROR_404 = ['title'=>'Page introuvable','icon'=>'icon-search-lg','message'=>"Il semblerait que la page que vous essayez d'atteindre n'existe pas ou a été déplacée !<br>Cliquez sur le bouton ci-dessous pour retourner à l'accueil."];
-
     public function __construct(NotificationService $notificationService)
     {
         $this->notificationService = $notificationService;
@@ -27,7 +23,7 @@ class ExceptionListener
                 'url' => $_SERVER['SERVER_NAME'],
                 'code' => $event->getThrowable()->getCode(),
                 'error' => $event->getThrowable()->getMessage(),
-                'signalement' => var_dump($event->getRequest()->get('signalement'))
+                'signalement' => $event->getRequest()->get('signalement')
             ]);
         }
     }
